5ea73320b133e00f5470830ac8f1da722fd0ef6c
AnhhTuann <tuannguyen25101999@gmail.com>
2024-12-24 00:49:29 +0700

    completed header

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,28 @@
-/*===========  MENU SHOW ==========*/
+/*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
-/*===== MENU SHOW =====*/
+
+/* Menu show */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
-/*==== MENU HIDDEN =====*/
+
+/* Menu hidden */
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 }
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
